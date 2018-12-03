34d45a2d82c931068e6cb98d7739835d4d7359d4
Graham Holtslander <gholtslander@vendasta.com>
2018-12-03 10:58:10 -0600

    PEP-8 compliant python template

--- cmd/start/python.go
+++ cmd/start/python.go
@@ -23,11 +23,13 @@ import (
 const (
 	pythonTemplate = `""" Advent of Code """
 
+
 def main():
     """ Process advent of code """
     parts = open('./input.txt')
     for part in parts:
         print part,
+
 
 if __name__ == '__main__':
     main()
